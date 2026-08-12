d2fb8e3d4c30747dee92f8b586ad42697ac59251
Gyaneshwar Sharma <gs.mcse2003@gmail.com>
2026-08-12 23:07:47 +0530

    Add dotenv support for MongoDB URI
    Load environment variables and configure MongoDB connection.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 import json
 from flask import Flask, jsonify, request, render_template
+from dotenv import load_dotenv
 from pymongo import MongoClient
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=10000)
 
 app = Flask(__name__)
 
