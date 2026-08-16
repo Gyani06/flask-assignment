996412ea63c07bca394757282036518d9e17114a
Gyaneshwar Sharma <gs.mcse2003@gmail.com>
2026-08-16 19:03:37 +0530

    Add todo_items helper class and submit route
    Added a helper class for managing todo items in MongoDB, including methods for creating, retrieving, updating, and deleting items. Implemented a new API route to submit todo items with additional fields.

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import json
 from flask import Flask, jsonify, request, render_template
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import certify
 
 app = Flask(__name__, template_folder='template')
 
@@ -21,9 +20,66 @@ client = MongoClient(
 db = client.todo_db
 collection = db.todo_items
 
+class todo_items:
+    """Helper class to manage todo items in MongoDB."""
+
+    def __init__(self, collection):
+        self.collection = collection
+
+    def create_item(self, item_name, item_description):
+        """Insert a new todo item and return the inserted id."""
+        doc = {
+            "itemName": item_name,
+            "itemDescription": item_description
+        }
+        result = self.collection.insert_one(doc)
+        return str(result.inserted_id)
+
+    def get_all(self):
+        """Return all todo items as a list of dicts."""
+        items = []
+        for doc in self.collection.find():
+            doc['_id'] = str(doc.get('_id'))
+            items.append(doc)
+        return items
+
+    def get_by_id(self, id_):
+        """Return a single todo item by its id or None."""
+        from bson.objectid import ObjectId
+        try:
+            doc = self.collection.find_one({"_id": ObjectId(id_)})
+        except Exception:
+            return None
+        if doc:
+            doc['_id'] = str(doc.get('_id'))
+        return doc
+
+    def update_item(self, id_, data):
+        """Update fields of a todo item. Returns True if modified."""
+        from bson.objectid import ObjectId
+        try:
+            result = self.collection.update_one({"_id": ObjectId(id_)}, {"$set": data})
+        except Exception:
+            return False
+        return result.modified_count > 0
+
+    def delete_item(self, id_):
+        """Delete a todo item by id. Returns True if deleted."""
+        from bson.objectid import ObjectId
+        try:
+            result = self.collection.delete_one({"_id": ObjectId(id_)})
+        except Exception:
+            return False
+        return result.deleted_count > 0
+
+# instantiate helper
+todo_helper = todo_items(collection)
+
+# Existing API Route
 @app.route('/')
 def home():
     return render_template('todo.html')
+
 
 # Existing API Route
 @app.route('/api')
@@ -31,8 +87,10 @@ def get_data():
     here = os.path.dirname(__file__)
     path = os.path.join(here, 'data.json')
 
+
     with open(path, 'r', encoding='utf-8') as f:
         data = json.load(f)
+
 
     return jsonify(data)
 
@@ -40,17 +98,32 @@ def get_data():
 @app.route('/submittodoitem', methods=['POST'])
 def submit_todo():
 
-    item_name = request.form.get('itemName')
-    item_desc = request.form.get('itemDescription')
+    print("POST /submittodoitem received")
 
-    collection.insert_one({
+    item_name = request.form.get('itemName')
+    item_id = request.form.get('itemID')
+    item_uuid = request.form.get('itemUUID')
+    item_hash = request.form.get('itemHash')
+    item_description = request.form.get('itemDescription')
+
+    print("Item Name:", item_name)
+    print("Item ID:", item_id)
+    print("Item UUID:", item_uuid)
+    print("Item Hash:", item_hash)
+
+    inserted_id = collection.insert_one({
         "itemName": item_name,
-        "itemDescription": item_desc
-    })
+        "itemID": item_id,
+        "itemUUID": item_uuid,
+        "itemHash": item_hash,
+        "itemDescription": item_description
+    }).inserted_id
 
     return jsonify({
-        "message": "Todo item saved successfully"
+        "message": "Todo item saved successfully",
+        "id": str(inserted_id)
     })
+
 # MongoDB code here
 
     return jsonify({
@@ -59,3 +132,4 @@ def submit_todo():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+    
